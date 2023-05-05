162e89787548c7387d097fc7e5e62a145026d0ad
sky-beta <126281284+sky-beta@users.noreply.github.com>
2023-05-05 21:34:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname="https://nsp-game-jxs-p01-drcn.obs.cn-north-2.myhuaweicloud.com"; //beta.radiance.thatgamecompany.com
+      url.hostname="https://live.com"; //beta.radiance.thatgamecompany.com
       let new_request=new Request(url,request);
       return fetch(new_request);
     }
